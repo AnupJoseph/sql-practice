58895c428b4766c855078d9e058868a2873fbde6
Anup Joseph <anup.joseph@quantiphi.com>
2022-06-30 10:09:37 +0530

    Finished the Top Three salaries questions with a nice CTE

--- medium_datasets/Top_Three_Salaries/query.sql
+++ medium_datasets/Top_Three_Salaries/query.sql
@@ -1,25 +1,32 @@
 SELECT
-  e.first_name,
-  e.last_name,
-  d.name,
-  e.salary
+  CONCAT(first_name," ",last_name) AS full_name,
+  name AS department_name,
+  salary
 FROM (
   SELECT
-    department_id,
-    first_name,
-    last_name,
-    salary
-  FROM
-    `ringed-bond-261712.medium_questions_dataset.employees`
-  GROUP BY
-    department_id,
-    first_name,
-    last_name,
-    salary
-  ORDER BY
-    salary DESC
-  ) AS e
-INNER JOIN
-  `ringed-bond-261712.medium_questions_dataset.departments` AS d
-ON
-  e.department_id = d.id
+    *
+  FROM (
+    WITH
+      cte AS (
+      SELECT
+        first_name,
+        last_name,
+        department_id,
+        salary,
+        ROW_NUMBER() OVER (PARTITION BY department_id ORDER BY salary DESC ) AS rn
+      FROM
+        `ringed-bond-261712.medium_questions_dataset.employees` )
+    SELECT
+      first_name,
+      last_name,
+      department_id,
+      rn,
+      salary
+    FROM
+      cte
+    WHERE
+      rn <=3) AS target_employees
+  INNER JOIN
+    `ringed-bond-261712.medium_questions_dataset.departments` AS d
+  ON
+    target_employees.department_id = d.id)
