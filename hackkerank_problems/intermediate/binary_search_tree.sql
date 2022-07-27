e91212817eb73fb38af3d1b6a2d7889551a66fa4
Anup Joseph <anup.joseph@quantiphi.com>
2022-07-27 18:03:15 +0530

    Added BST

---
+++ hackkerank_problems/intermediate/binary_search_tree.sql
@@ -0,0 +1,11 @@
+/*
+Enter your query here.
+*/
+SELECT N,
+    CASE
+        WHEN P IS NULL THEN 'Root'
+        WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+        ELSE "Leaf"
+    END
+FROM BST
+ORDER BY N
